dacfdad64f6a47644946db9cca74f305787d140b
Gabriel Reader <gabelol@Gabriels-MBP.attlocal.net>
2020-12-05 11:58:01 -0800

    added function

--- BabasMoney.py
+++ BabasMoney.py
@@ -5,3 +5,13 @@ t = float(input("How many payments per period? "))
 pv = p * (pow((1 + r/100/n),n*t))
 
 print (pv)
+
+def compound_interest(p,r,n,t):
+    balance = p * (pow((1 + r/100/n),n*t))
+    ci = balance - p
+    print("Balance at the end of the period ", round(balance,2))
+    print("Interest earned is ", round(ci,2))
+
+compoun_interest(5000,10,12,5)
+
+
