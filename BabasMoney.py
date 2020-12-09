6cdd31421c6e90aace8ec4e543d5cb1a1b2a1901
Gabriel Reader <gabelol@Gabriels-MBP.attlocal.net>
2020-12-08 20:36:19 -0800

    modified ROI calculator

--- BabasMoney.py
+++ BabasMoney.py
@@ -11,17 +11,27 @@ roi = return on investment
 """
 def compound_interest():
     p = float(input("How much is your present value? $"))
-    r = float(input("What's the rate? "))
+    r = float(input("What's the rate? % = "))
     n = float(input("How many periods per years? "))
     t = int(input("How many years? "))
+    
     balance = p * (pow((1 + r/100/n),n*t))
     earnings = balance - p
     roi = earnings / balance * 100
     
-    print("You earned $", round(earnings,2),"during the ",t," year period. ")
-    print("Balance at the end of the period  = $", round(balance,2))
-    print("This is a ",round(roi,2),"% return on investment")
+    print("\nYou earned $", round(earnings,2),"during this",t,"year period. ")
+    print("Balance at the end of the period = $", round(balance,2))
+    print("This is a",round(roi,2),"% return on investment")
 
 compound_interest()
+
+"""
+roi = x * earnings
+
+roi / earnings = x * earnings / earnings
+
+_____ = x
+
+"""
 
 
